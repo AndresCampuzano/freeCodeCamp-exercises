9178ffdbe29e9cf75d6c17447a521aa50a6fba01
unknown <campuxano88@gmail.com>
2019-12-29 08:12:14 -0500

    iterating with loops - for

---
+++ b-js-loops.js
@@ -0,0 +1,19 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 1; i <= 5; i++) {
+  myArray.push(i);
+  console.log(myArray)
+}
+
+// Only change code below this line.
+
+//>>>>> myArray should equal [1,2,3,4,5].
+

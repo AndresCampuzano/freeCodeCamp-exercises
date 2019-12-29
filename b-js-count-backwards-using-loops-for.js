5be09bd114eb78ff8fe97ca98f466f26c5ec15cc
unknown <campuxano88@gmail.com>
2019-12-29 08:36:34 -0500

    Updating lessons learned

---
+++ b-js-count-backwards-using-loops-for.js
@@ -0,0 +1,21 @@
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 9; i >= 1; i -=2) {
+  myArray.push(i);
+  console.log(myArray)
+}
+
+// Only change code below this line.
+
+
+
+// >>> myArray should equal [9,7,5,3,1].
+
---
+++ bs-js-odd-number-with-loop.js
@@ -0,0 +1,19 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 1; i <= 9; i+=2) {
+  myArray.push(i);
+  console.log(myArray)
+}
+
+// Only change code below this line.
+
+
+// >>>>> myArray should equal [1,3,5,7,9].

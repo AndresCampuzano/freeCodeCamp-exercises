13cb508ddfbe75f5a8574b3f401116af1e70e02e
unknown <campuxano88@gmail.com>
2019-12-30 16:23:11 -0500

    Learning do while loops.

---
+++ b-js-do-while.js
@@ -0,0 +1,16 @@
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line
+
+do {
+    myArray.push(i);
+  i++;
+} while (i < 5)
+
+// You should be using a do...while loop for this exercise.
+// Passed
+// myArray should equal [10].
+// Passed
+// i should equal 11

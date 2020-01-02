baf4181c4042210d1b5e6af97e5b44d90c985d22
unknown <campuxano88@gmail.com>
2020-01-02 06:51:31 -0500

    Learning conditional ? and parseInt.

---
+++ b-js-conditional-two-dots.js
@@ -0,0 +1,17 @@
+function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal"
+}
+
+checkEqual(1, 2);
+
+
+// checkEqual should use the conditional operator
+// Passed
+// checkEqual(1, 2) should return "Not Equal"
+// Passed
+// checkEqual(1, 1) should return "Equal"
+// Passed
+// checkEqual(1, -1) should return "Not Equal"
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-conditional-ternary-operator
---
+++ b-js-using-parseInt-to-convert-str-to-numbers.js
@@ -0,0 +1,20 @@
+function convertToInteger(str) {
+    return parseInt(str)
+   }
+   
+   convertToInteger("56");
+   
+
+
+//    convertToInteger should use the parseInt() function
+// Passed
+// convertToInteger("56") should return a number
+// Passed
+// convertToInteger("56") should return 56
+// Passed
+// convertToInteger("77") should return 77
+// Passed
+// convertToInteger("JamesBond") should return NaN
+
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/use-the-parseint-function

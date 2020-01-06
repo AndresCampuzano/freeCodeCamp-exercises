9f7f425f8dd3d1c213534e823fec5b519cd627b0
unknown <campuxano88@gmail.com>
2020-01-06 00:08:52 -0500

    Learnend pop, concat and indexof.

---
+++ js-hero-concat.js
@@ -0,0 +1,6 @@
+// Write a function concatUp that concatenate two arrays. The longer array should be appended to the shorter array. If both arrays are equally long, the second array should be appended to the first array. concatUp([1, 2], [3]) should return [3, 1, 2] and concatUp([5, 7], [6, 8]) should return [5, 7, 6, 8].
+
+
+function concatUp (par1, par2) {
+    
+}
---
+++ js-hero-indexof.js
@@ -0,0 +1,46 @@
+// Write a function add that adds an element to the end of an array. However, the element should only be added if it is not already in the array. add([1, 2], 3) should return [1, 2, 3] and add([1, 2], 2) should return [1, 2].
+
+
+// THERE ARE TWO WAYS
+
+// HARD WAY:
+
+ function add (par1, par2) {
+     var myArray = [];
+     myArray.push(par1);
+     myArray.push(par2);
+     // [ [ 'J' ], 'S' ]
+      if (myArray[0].indexOf(par2) === -1) {
+         myArray[0].push(par2); // [ [ 'J', 'S' ], 'S' ]
+         myArray.pop(); // [ [ 'J', 'S' ] ]
+         return myArray[0];
+      }    else {
+          myArray = par1;
+          return myArray
+      }
+ }
+
+
+
+ add(['J'], 'S') // returns [ 'J', 'S' ].
+
+ add(['J'], 'J') // returns [ 'J' ].
+
+ add([1, 2], 3) // returns [ 1, 2, 3 ].
+
+ add([1, 2], 2) // returns [ 1, 2 ].
+
+
+ // EASY WAY:
+
+
+// function add (par1, par2) {
+//     var myArray = par1
+//     if (myArray.indexOf(par2) === -1) {
+//         myArray.push(par2)
+//         return myArray
+//     } else {
+//         myArray = par1
+//         return myArray
+//     }
+// }

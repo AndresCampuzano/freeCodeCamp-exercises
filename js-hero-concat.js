ed6197868e23868141d1184c2b0c33ca0e28e4a7
unknown <campuxano88@gmail.com>
2020-01-06 00:09:50 -0500

    Saving last code done.

--- js-hero-concat.js
+++ js-hero-concat.js
@@ -2,5 +2,18 @@
 
 
 function concatUp (par1, par2) {
-    
+    var arrayPar1 = par1;
+    var arrayPar2 = par2;
+
+
+    if (arrayPar1.length > arrayPar2.length) {
+        var conc1 = arrayPar2.concat(arrayPar1);
+        return conc1;
+    } else if (arrayPar2.length > arrayPar1.length) {
+        var conc2 = arrayPar1.concat(arrayPar2);
+        return conc2;
+    } else if (arrayPar1.length === arrayPar2.length) {
+        var conc3 = arrayPar1.concat(arrayPar2)
+        return conc3
+    }
 }

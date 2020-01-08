f79f1c1e9e525a7d0b0f4d2a377f8dce5d9c82b1
unknown <campuxano88@gmail.com>
2020-01-08 04:27:16 -0500

    saving work.

--- js-hero-array of arrays.js
+++ js-hero-array of arrays.js
@@ -8,6 +8,7 @@
 
 function flat (par1) {
     var array = par1;
+
     var a = array[0];
     var b = array[1];
     var c = array[2];
@@ -16,3 +17,18 @@ function flat (par1) {
     var e = d.concat(c);
     return e;
 }
+
+
+// flat is a function.
+
+// flat has 1 parameter.
+
+flat([[], [], []]) // returns [].
+
+flat([[1], [], []]) // returns [ 1 ].
+
+flat([[1], [2], [3]]) // returns [ 1, 2, 3 ].
+
+flat([[1, 2], [3, 4], [5, 6]]) // returns [ 1, 2, 3, 4, 5, 6 ].
+
+flat([['a'], ['b', 'c'], ['d', 'e', 'f']]) // returns [ 'a', 'b', 'c', 'd', 'e', 'f' ].
---
+++ js-hero-finding-media-array.js
@@ -0,0 +1,3 @@
+// Write a function median that takes an array of ascending numbers and returns the median of that numbers. median([1, 2, 10]) should return 2 and median([1, 2, 10, 100]) should return 6.
+
+
